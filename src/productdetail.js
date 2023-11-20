6e869dc8ed0c25c78102a16bad0ef86b3a625b38
Lazarus-dev <135232185+Lazarus-dev@users.noreply.github.com>
2023-11-20 20:10:38 +0300

    first commit

---
+++ src/productdetail.js
@@ -0,0 +1,87 @@
+const Homeproduct=
+[
+    {
+        id:1,
+        Title:"Honey moon beds.",
+        Cat:'King size',
+        Price:'KSH.20000',
+        Img:process.env.PUBLIC_URL + '/img/bed 1.jpeg'
+    },
+    {
+        id:2,
+        Title:"Trundle bed.",
+        Cat:'Queen size',
+        Price:'KSH.30900',
+        Img:process.env.PUBLIC_URL + '/img/bed 2.jpeg'
+    },
+    {
+        id:3,
+        Title:"Murphy bed.",
+        Cat:'Divan',
+        Price:'KSH.25990',
+        Img:process.env.PUBLIC_URL + '/img/bed 3.jpeg'
+    },
+    {
+        id:4,
+        Title:"Storage bed.",
+        Cat:'Loft',
+        Price:'KSH.12990',
+        Img:process.env.PUBLIC_URL + '/img/bed 4.jpeg'
+    {
+        id:5,
+        Title:"Adjustable bed.",
+        Cat:'Panel',
+        Price:'KSH.16799',
+        Img:process.env.PUBLIC_URL + '/img/bed 5.jpeg'
+    },
+    {
+        id:6,
+        Title:"Sofa bed.",
+        Cat:'Innersping',
+        Price:'KSH.34000',
+        Img:process.env.PUBLIC_URL + '/img/bed 6.jpeg'
+    },
+    {
+        id:7,
+        Title:"Canopy bed.",
+        Cat:'Futon',
+        Price:'KSH.35088',
+        Img:process.env.PUBLIC_URL + '/img/bed 7.jpeg'
+    },
+    {
+        id:8,
+        Title:"Bunk bed.",
+        Cat:'Blackwo',
+        Price:'KSH.19990',
+        Img:process.env.PUBLIC_URL + '/img/bed 8.jpeg'
+    },
+    {
+        id:9,
+        Title:"Woven bed.",
+        Cat:'Boxen',
+        Price:'KSH.15000',
+        Img:process.env.PUBLIC_URL +'/img/bed 9.jpeg'
+    },
+    {
+        id:10,
+        Title:"Cottage bed.",
+        Cat:'Twin',
+        Price:'KSH.17999',
+        Img:process.env.PUBLIC_URL + '/img/bed 10.jpeg'
+    },
+    {
+        id:11,
+        Title:"Book case bed.",
+        Cat:'Sonic',
+        Price:'KSH.90000',
+        Img:process.env.PUBLIC_URL + '/img/bed 11.jpeg'
+    },
+    {
+        id:12,
+        Title:"Convertible bed.",
+        Cat:'Upholstered',
+        Price:'KSH.22088',
+        Img:process.env.PUBLIC_URL + '/img/bed 12.jpeg'
+    },
+]
+export default Productdetail;
